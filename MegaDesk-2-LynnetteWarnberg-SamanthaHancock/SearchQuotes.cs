2630e146b2c085e12866bb0fd2ce5834b058c5f7
samanthahancock <han13018@byui.edu>
2019-02-07 11:32:22 -0700

    Json convertor

--- MegaDesk-2-LynnetteWarnberg-SamanthaHancock/SearchQuotes.cs
+++ MegaDesk-2-LynnetteWarnberg-SamanthaHancock/SearchQuotes.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace MegaDesk_3_LynnetteWarnberg
 {
@@ -44,7 +45,7 @@ namespace MegaDesk_3_LynnetteWarnberg
                 foreach (string deskQuote in deskQuotes)
                 {
                     string[] arrRow = deskQuote.Split(new char[] { ',' });
-                    if (arrRow[5]==surfaceMaterial){
+                    if (arrRow[5] == surfaceMaterial) {
                         dataSearchAllQuotes.Rows.Add(arrRow);
                     }
 
@@ -59,19 +60,39 @@ namespace MegaDesk_3_LynnetteWarnberg
 
         private void CancelQuoteSearch_Click(object sender, EventArgs e)
         {
-           
+
             Close();
         }
-
-        private void SearchQuotes_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            var mainMenu = (MainMenu)Tag;
-            mainMenu.Show();
-        }
-
         private void getQuotes_Click(object sender, EventArgs e)
         {
             loadGrid();
         }
+        private void SearchQuotes_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var mainMenu = (MainMenu)Tag;
+            mainMenu.Show();
+        
+       var quotesFile = @"quotes.json"; 
+
+        using (StreamReader reader = new StreamReader(quotesFile))
+            {
+                string quotes = reader.ReadToEnd();
+    List<DeskQuote> allQuotes = JsonConvert.DeserializeObject<List<DeskQuote>>(quotes);
+
+    dataSearchAllQuotes.DataSource = allQuotes.Select(d => new
+        {
+            QuoteDate = d.QuoteDate,
+            CustomerName = d.CustomerName,
+            Width = d.Desk.Width,
+            Depth = d.Desk.Depth,
+            NumberOfDrawers = d.Desk.NumberOfDrawers,
+            SurfaceMaterial = d.Desk.SurfaceMaterial,
+            DeliveryType = d.DeliveryType,
+            QuoteAmount = d.QuoteAmount
+        })
+        .Where(q => q.SurfaceMaterial == (Desk.Surface)surfaceMaterialValues.SelectedValue)
+        .ToList();
+        }
+       }
     }
 }
--- MegaDesk-2-LynnetteWarnberg-SamanthaHancock/ViewAllQuotes.cs
+++ MegaDesk-2-LynnetteWarnberg-SamanthaHancock/ViewAllQuotes.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace MegaDesk_3_LynnetteWarnberg
 {
@@ -49,6 +50,26 @@ namespace MegaDesk_3_LynnetteWarnberg
         {
             var mainMenu = (MainMenu)Tag;
             mainMenu.Show();
+
+            var quotesFile = @"quotes.json";
+
+            using (StreamReader reader = new StreamReader(quotesFile))
+            {
+                string quotes = reader.ReadToEnd();
+                List<DeskQuote> allQuotes = JsonConvert.DeserializeObject<List<DeskQuote>>(quotes);
+
+                dataViewAllQuotes.DataSource = allQuotes.Select(d => new
+                {
+                    QuoteDate = d.QuoteDate,
+                    CustomerName = d.CustomerName,
+                    Width = d.Desk.Width,
+                    Depth = d.Desk.Depth,
+                    NumberOfDrawers = d.Desk.NumberOfDrawers,
+                    SurfaceMaterial = d.Desk.SurfaceMaterial,
+                    DeliveryType = d.DeliveryType,
+                    QuoteAmount = d.QuoteAmount
+                }).ToList();
+            }
         }
     }
 }
